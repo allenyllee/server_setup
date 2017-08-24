c01ff229a00b49194955dd23a439f132b555c408
Allencat Lee <allencat.lee@liteon.com>
2017-08-24 18:37:31 +0800

    add backup script

---
+++ backup.sh
@@ -0,0 +1,56 @@
+BACKUPDIR=file:///media/allencat/TODS512G/server_backup
+#BACKUPDIR=file:///media/allencat/ALN16G01/backup
+
+SOURCEDIR=/
+FULLBACKUPTIME=1M
+
+FILEDIR="/home/allencat/docker/server_setup"
+INCLUDELIST=$FILEDIR/backup_list.txt
+LOGFILE=$FILEDIR/backup.log
+FULLBACKUPTIME=1M
+
+function dpremove {
+  duplicity remove-all-but-n-full 1 \
+            --no-encryption \
+            --force \
+            --log-file $LOGFILE \
+            $BACKUPDIR
+}
+
+function dpclean {
+  duplicity cleanup \
+            --no-encryption \
+            --force \
+            --log-file $LOGFILE \
+            $BACKUPDIR
+}
+
+function dpbackup {
+  duplicity --no-encryption \
+          --full-if-older-than $FULLBACKUPTIME \
+          --progress \
+          --verbosity 8 \
+          --log-file $LOGFILE \
+          --include-filelist $INCLUDELIST \
+          $SOURCEDIR $BACKUPDIR
+}
+
+
+for (( c=1; c<=2; c++))
+do
+  echo loop $c
+
+  rm -f $LOGFILE
+  dpbackup
+
+  if [ $? -eq 53 ] #dosen't have enough space
+  then #remove & clean and try again
+    dpremove
+    dpclean
+  else
+    break
+  fi
+
+done
+
+
