330292717154978fb6b151870ea03a5a2e7fb8e6
Allencat Lee <allencat.lee@liteon.com>
2017-08-30 12:33:45 +0800

    modify run docker squid script to more readable

--- run_squid_proxy_server.sh
+++ run_squid_proxy_server.sh
@@ -1,4 +1,8 @@
-docker run --name squid -d --restart=always \
+#!/bin/bash
+
+docker run -d \
+  --name squid \
+  --restart=always \
   --publish 3128:3128 \
   --volume /srv/docker/squid/cache:/var/spool/squid3 \
   sameersbn/squid:latest
