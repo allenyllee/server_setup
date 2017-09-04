3f73c1d5ac53dbb19d3da4909d1fe4d70d164dac
Allencat Lee <allencat.lee@liteon.com>
2017-09-04 18:04:04 +0800

    add portainer & registry

---
+++ run_docker_protainer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker container run -d \
+                     -p 9000:9000 \
+                     -v /var/run/docker.sock:/var/run/docker.sock \
+                     portainer/portainer
--- run_docker_registry_server.sh
+++ run_docker_registry_server.sh
@@ -3,6 +3,6 @@
 docker run -d \
     -p 5000:5000 \
     --restart=always \
-    --name registry-srv \
+    --name registry \
     -v /mnt/docker_registry:/var/lib/registry \
     registry:2
