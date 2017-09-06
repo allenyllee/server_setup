4893a37f4257be622c51501727cde4de464275b2
Allencat Lee <allencat.lee@liteon.com>
2017-09-06 11:33:23 +0800

    script for running docker registry backend & frontend and create a network to connect it

---
+++ run_docker_registry.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+NETWORK=reg_net
+BACKEND=reg-server
+BACKENDPORT=5000
+FRONTEND=reg-ui
+FRONTENDPORT=8080
+
+#
+# create a network bridge between containers
+#
+docker network create \
+  --driver=bridge \
+  $NETWORK
+
+#
+# run a docker registry backend container and link it to network bridge above
+#
+docker run \
+  --detach \
+  -p $BACKENDPORT:5000 \
+  --restart=always \
+  --name=$BACKEND \
+  --network=$NETWORK \
+  -v /mnt/docker_registry:/var/lib/registry \
+  registry:2
+
+#
+# run a docker registry frontend container and link it to network bridge above
+#
+docker run \
+  --detach \
+  -e ENV_DOCKER_REGISTRY_HOST=$BACKEND \
+  -e ENV_DOCKER_REGISTRY_PORT=$BACKENDPORT \
+  -p $FRONTENDPORT:80 \
+  --network=$NETWORK \
+  --name=$FRONTEND \
+  --restart=always \
+  konradkleine/docker-registry-frontend:v2
+
+
+
--- run_docker_registry_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run -d \
-    -p 5000:5000 \
-    --restart=always \
-    --name registry \
-    -v /mnt/docker_registry:/var/lib/registry \
-    registry:2
