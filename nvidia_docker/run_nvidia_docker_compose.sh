82e42d3644694e7ea8854cdc04f1b7f4c5fd1d98
Allencat Lee <allencat.lee@liteon.com>
2017-09-22 16:22:41 +0800

    solve library link issue & X window GUI display issue

--- nvidia_docker/run_nvidia_docker_compose.sh
+++ nvidia_docker/run_nvidia_docker_compose.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
-nvidia-docker-compose run cuda bash
+
+##
+## Linux xhost command help and examples
+## https://www.computerhope.com/unix/xhost.htm
+##
+## xhost [[+-]name ...]
+##
+## [+]name 	The given name (the plus sign is optional) is added to the list allowed to connect to the X server. The name can be a host name or a complete name
+## -name 	The given name is removed from the list of allowed to connect to the server. The name can be a host name or a complete name 
+##
+## Names
+## 
+## A complete name has the syntax "family:name" where the families are as follows:
+## inet 	Internet host (IPv4).
+## inet6 	Internet host (IPv6).
+## dnet 	DECnet host.
+## nis 	Secure RPC network name.
+## krb 	Kerberos V5 principal.
+## local 	Contains only one name, the empty string.
+## si 	Server Interpreted.
+##
+##
+## x11 - Running Chromium inside Docker - Gtk: cannot open display: :0 - Stack Overflow
+## https://stackoverflow.com/questions/28392949/running-chromium-inside-docker-gtk-cannot-open-display-0
+##
+xhost local:root
+
+nvidia-docker-compose build
+nvidia-docker-compose run cuda bash -c "cd ~/project;bash"
