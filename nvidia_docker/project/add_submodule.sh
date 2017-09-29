5611236cd58fc5ef9077cf542063f024554e8a6b
Allencat Lee <allencat.lee@liteon.com>
2017-09-29 17:44:57 +0800

    add submodules

---
+++ nvidia_docker/project/add_submodule.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#
+# add items that you want it be a submodule
+#
+git submodule add https://github.com/allenyllee/jupyter.git
+git submodule add https://github.com/allenyllee/CUDA_Samples_for_Linux.git
+git submodule add https://github.com/allenyllee/pix2pix-tensorflow.git
+
+
