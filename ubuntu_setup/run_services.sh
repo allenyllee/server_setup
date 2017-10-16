922d34c1e744531aaefa7948dccbb832d8414648
allenyllee <allen7575@gmail.com>
2017-10-16 16:59:47 +0800

    add nvidia docker compose

--- ubuntu_setup/run_services.sh
+++ ubuntu_setup/run_services.sh
@@ -6,31 +6,63 @@ DOCKERDIR="/mnt/docker-srv"
 sudo mkdir -p $DOCKERDIR
 sudo chmod 777 $DOCKERDIR
 
+#
+# gitlab data
+#
 GITLAB_DIR=$DOCKERDIR/gitlab
 export GITLAB_DIR
 
+#
+# jenkins data
+#
 JENKINS_DIR=$DOCKERDIR/jenkins_home
 export JENKINS_DIR
 
+#
+# docker registry data
+#
 REGISTRY_DIR=$DOCKERDIR/registry
 export REGISTRY_DIR
 
 REG_BACKENDPORT=5000
 export REG_BACKENDPORT
 
+#
+# squid proxy data
+#
 SQUID_DIR=$DOCKERDIR/squid/cache
 export SQUID_DIR
 
+#
+# samba server shared folder
+#
 SAMBA_DIR=$DOCKERDIR/samba
 export SAMBA_DIR
 
+#
+# xtensa source
+#
 XTENSA_GIT=$PROJECT_DIR/xtensa_X_docker
 export XTENSA_GIT
 
+# folder to share with samba
 XTENSA_DIR=$SAMBA_DIR/project
 export XTENSA_DIR
 
+###################
+# docker
+###################
 
-
-# starts the containers in the background and leaves them running
+# starting docker containers in the background and leaves them running
 docker-compose up -d
+
+###################
+# nvidia docker
+###################
+
+# clone submodules
+bash ../nvidia_docker/project/init_submodule.sh
+
+# starting nvidia docker containers in the background and leaves them running
+nvidia-docker-compose -f ../nvidia_docker/docker-compose.yml up -d
+
