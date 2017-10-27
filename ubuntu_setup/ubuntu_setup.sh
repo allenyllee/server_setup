5ed280b4d8843582a758b3bdcd4d8b77919f0491
allenyllee <allen7575@gmail.com>
2017-10-27 22:05:31 +0800

    add comments

--- ubuntu_setup/ubuntu_setup.sh
+++ ubuntu_setup/ubuntu_setup.sh
@@ -334,6 +334,9 @@ sudo dpkg -i bitmeteros_*-amd64.deb
 ###############
 # install google chrome
 ###############
+# UbuntuUpdates - PPA: Google Chrome 
+# https://www.ubuntuupdates.org/ppa/google_chrome
+#
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update 
