e06773d7d8e28ba9960fd5983114397b32320d90
allenyllee <allen7575@gmail.com>
2017-10-28 23:15:21 +0800

    Running PulseEffects in the background after close its frontend window

--- ubuntu_setup/pulseeffects.sh
+++ ubuntu_setup/pulseeffects.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=pulseeffects com.github.wwmm.pulseeffects -s -n &
+
+#
+# linux - Prevent process from killing itself using pkill - Stack Overflow 
+# https://stackoverflow.com/questions/15740481/prevent-process-from-killing-itself-using-pkill
+#
+echo "pid of this process is $$, kill all matched process but pid=$$"
+kill $(pgrep -f pulseeffects | grep -v ^$$\$)
+
+/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=pulseeffects com.github.wwmm.pulseeffects -s &>/dev/null
+/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=pulseeffects com.github.wwmm.pulseeffects -s -n &>/dev/null &
