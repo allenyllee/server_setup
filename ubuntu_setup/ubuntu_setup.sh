545b36d027c7259186e3a2876c27cbf7276494f7
allenyllee <allen7575@gmail.com>
2017-11-11 01:31:29 +0800

    auto generate xauth when system boot

--- ubuntu_setup/ubuntu_setup.sh
+++ ubuntu_setup/ubuntu_setup.sh
@@ -108,12 +108,13 @@ sudo service docker restart
 # 
 
 # workflow:
-#       1. To avoid docker daemon automatically allocate .docker.xauth as a directory, 
-#           insert item into /etc/rc.local to create a placeholder file .docker.xauth and change owner to 1000 (default user)
-#           Because /etc/rc.local will execute at the end of runlevel, before system service start, 
-#           this is a good point to create this file.
-#       2. After docker daemon start, it will mount .docker.xauth if needed. 
-#       3. After login, system will execute ~/.profile to setup .docker.xauth file
+#       1. To avoid docker daemon automatically create a /tmp/.docker.xauth/ directory, 
+#           insert a item which is to create /temp/.docker.xauth/ directory 
+#           with mod 777 (read/write for all user) into /etc/rc.local.
+#           Because /etc/rc.local will execute at the end of runlevel which before docker service start, 
+#           this is a good point to place it.
+#       2. After docker daemon start, it will mount /tmp/.docker.xauth/ if needed. 
+#       3. After login, system will execute ~/.profile to setup /tmp/.docker.xauth/.xauth file
 
 # 1. Use tr to swap the newline character to NUL character.
 #       NUL (\000 or \x00) is nice because it doesn't need UTF-8 support and it's not likely to be used.
@@ -121,18 +122,21 @@ sudo service docker restart
 # 3. Use tr to swap back.
 # 4. insert a string into /etc/rc.local before exit 0
 tr '\n' '\000' < /etc/rc.local | sudo tee /etc/rc.local >/dev/null
-sudo sed -i 's|\x00XAUTH=.*\x00\x00|\x00|' /etc/rc.local >/dev/null
+sudo sed -i 's|\x00XAUTH_DIR=.*\x00\x00|\x00|' /etc/rc.local >/dev/null
 tr '\000' '\n' < /etc/rc.local | sudo tee /etc/rc.local >/dev/null
-sudo sed -i 's|^exit 0.*$|XAUTH=/tmp/.docker.xauth; if [ -d $XAUTH ]; then rm -rf $XAUTH; fi; touch $XAUTH; chown 1000 $XAUTH\n\nexit 0|' /etc/rc.local
+sudo sed -i 's|^exit 0.*$|XAUTH_DIR=/tmp/.docker.xauth; rm -rf $XAUTH_DIR; install -m 777 -d $XAUTH_DIR\n\nexit 0|' /etc/rc.local
+
+# create a folder with mod 777 that can allow all other user read/write
+XAUTH_DIR=/tmp/.docker.xauth; sudo rm -rf $XAUTH_DIR; install -m 777 -d $XAUTH_DIR
 
 # append string in ~/.profile
 tr '\n' '\000' < ~/.profile | sudo tee ~/.profile >/dev/null
-sed -i 's|\x00XAUTH=.*-\x00|\x00|' ~/.profile
+sed -i 's|\x00XAUTH_DIR=.*-\x00|\x00|' ~/.profile
 tr '\000' '\n' < ~/.profile | sudo tee ~/.profile >/dev/null
-echo "XAUTH=/tmp/.docker.xauth; touch \$XAUTH; xauth nlist \$DISPLAY | sed -e 's/^..../ffff/' | xauth -f \$XAUTH nmerge -" >> ~/.profile
+echo "XAUTH_DIR=/tmp/.docker.xauth; XAUTH=.xauth; touch \$XAUTH_DIR/\$XAUTH; xauth nlist \$DISPLAY | sed -e 's/^..../ffff/' | xauth -f \$XAUTH_DIR/\$XAUTH nmerge -" >> ~/.profile
 source ~/.profile
 
-#
+
 # install jq
 # https://stedolan.github.io/jq/
 # jq is like sed for JSON data - you can use it to slice and filter and 
