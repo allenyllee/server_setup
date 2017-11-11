04dcf385712bb7c7fb54797ce863199c384e31a9
allenyllee <allen7575@gmail.com>
2017-11-11 22:30:48 +0800

    some improvement

--- run_xeyes.sh
+++ run_xeyes.sh
@@ -4,17 +4,18 @@
 # x11 - Can you run GUI apps in a docker container? - Stack Overflow 
 # https://stackoverflow.com/a/25280523/1851492
 # 
-docker build -t xeyes - << EOF
+docker build -t xeyes - << __EOF__
 FROM debian
 RUN apt-get update
 RUN apt-get install -qqy x11-apps
-ENV DISPLAY :0
 CMD xeyes
-EOF
-
+__EOF__
 
 XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth
+XAUTH_DIR=/tmp/.docker.xauth
+XAUTH=$XAUTH_DIR/.xauth
+
+mkdir -p $XAUTH_DIR && touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker run -ti -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH xeyes
+docker run -ti -v $XSOCK:$XSOCK -v $XAUTH_DIR:$XAUTH_DIR -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH xeyes
 
