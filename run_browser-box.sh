76ec8f0c82bda1b279f2abf6db8af5d315085ad0
allenyllee <allen7575@gmail.com>
2017-11-11 23:02:34 +0800

    TODO: test GPU accelerate

--- run_browser-box.sh
+++ run_browser-box.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# how can you set up a docker container that acces web cams - Google 網上論壇
+# https://groups.google.com/forum/#!msg/docker-user/3o7ASnyeULA/b7rKHTITEQAJ
+# 
+# This is an example from a shell script we use to run X11 NVIDIA accellerated docker images:
+# 
+# sameersbn/docker-browser-box: Dockerized google-chome and tor-browser with audio support via pulseaudio 
+# https://github.com/sameersbn/docker-browser-box#getting-started
+# 
+# sameersbn/browser-box - Docker Hub 
+# https://hub.docker.com/r/sameersbn/browser-box/
+
 XSOCK=/tmp/.X11-unix
 XAUTH_DIR=/tmp/.docker.xauth
 XAUTH=$XAUTH_DIR/.xauth
@@ -7,16 +18,14 @@ XAUTH=$XAUTH_DIR/.xauth
 mkdir -p $XAUTH_DIR && touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
+# TODO: 
+# test GPU accelerate
+# 
 docker run -it --rm \
-    --privileged \
-    --cap-add=SYS_ADMIN \
     --env="USER_UID=$(id -u $USER)" \
     --env="USER_GID=$(id -g $USER)" \
     --env="DISPLAY=${DISPLAY}" \
     --env="XAUTHORITY=${XAUTH}" \
-    --device=/dev/dri/card0 \
-    --device=/dev/nvidia0 \
-    --device=/dev/nvidiactl \
     --volume=${XSOCK}:${XSOCK} \
     --volume=${XAUTH_DIR}:${XAUTH_DIR} \
     --volume=/run/user/$(id -u $USER)/pulse:/run/pulse \
