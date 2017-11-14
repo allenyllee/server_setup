8e625f2a657613d10962559f6468bfbe3f3ae1c3
allenyllee <allen7575@gmail.com>
2017-11-14 14:22:43 +0800

    comment out /etc/apt/sources.list.d/google.list, there are already have /etc/apt/sources.list.d/google-chrome.list

--- ubuntu_setup/ubuntu_setup.sh
+++ ubuntu_setup/ubuntu_setup.sh
@@ -595,7 +595,7 @@ sudo dpkg -i bitmeteros_*-amd64.deb
 # https://www.ubuntuupdates.org/ppa/google_chrome
 #
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+#sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update 
 sudo apt-get install -y google-chrome-stable
 
