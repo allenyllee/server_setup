d45b95b6479d150573e900f38cc57fc9f3199da1
allenyllee <allen7575@gmail.com>
2017-11-20 16:51:54 +0800

    use parameter $1 as image name

--- Dockerfile/template/run.sh
+++ Dockerfile/template/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run allenyllee/template $@
+# shell - Process all arguments except the first one (in a bash script) - Stack Overflow
+# https://stackoverflow.com/questions/9057387/process-all-arguments-except-the-first-one-in-a-bash-script
+
+docker run $1 ${@:2}
