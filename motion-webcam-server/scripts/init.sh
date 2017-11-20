6b427e91ab826ae745d818e49cbd800c23323b93
allenyllee <allen7575@gmail.com>
2017-11-20 17:37:22 +0800

    setup source dir

--- motion-webcam-server/scripts/init.sh
+++ motion-webcam-server/scripts/init.sh
@@ -2,6 +2,9 @@
 
 echo "input parameter: $@"
 
-./setup_motion.sh
+# source directory of this script
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+$SOURCE_DIR/setup_motion.sh
 
 bash
--- motion-webcam-server/scripts/setup_motion.sh
+++ motion-webcam-server/scripts/setup_motion.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# source directory of this script
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # sudo apt-get install motion
 
 # docker-webcam-server/motion.conf at master · johndevs/docker-webcam-server
 # https://github.com/johndevs/docker-webcam-server/blob/master/conf/motion.conf
 # sudo cp motion.conf /etc/motion/motion.conf
 
-# cp motion.conf /usr/local/etc/motion/
+cp $SOURCE_DIR/motion.conf /usr/local/etc/motion/
