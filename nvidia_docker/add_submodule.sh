6c24217fcfbd997e5cc47995510c1ca054b747f5
allenyllee <allen7575@gmail.com>
2017-11-24 00:20:22 +0800

    change name to cuda80-fix

--- nvidia_docker/add_submodule.sh
+++ nvidia_docker/add_submodule.sh
@@ -3,5 +3,5 @@
 #
 # add items that you want it be a submodule
 #
-git submodule add https://github.com/allenyllee/cuda80.git
+git submodule add https://github.com/allenyllee/cuda80-fix.git
 git submodule add https://github.com/allenyllee/cuda90.git
