7c1c89001f9744ba7bf3054db0f5a9460ed1d9f5
allenyllee <allen7575@gmail.com>
2017-12-01 01:41:52 +0800

    add ppt2pdf & fetch_gdrive_file

--- gist/add_submodule.sh
+++ gist/add_submodule.sh
@@ -5,3 +5,5 @@
 #
 git submodule add https://gist.github.com/c764c86ed722417948fc256b7a5077c4.git ./import_json_appsscript
 git submodule add https://gist.github.com/43dc25a18ac687cfea7f5a64707b9a83.git ./my_investment
+git submodule add https://gist.github.com/5d7c4a16ae0e33375e4a6d25acaeeda2.git ./ppt2pdf
+git submodule add https://gist.github.com/ac42730e9c756e0aeb9b2f07424b68dd.git ./fetch_gdrive_file
