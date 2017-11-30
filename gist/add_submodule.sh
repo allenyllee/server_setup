9cf16d78b2bb5ffda088c899d17e48c803810e3c
allenyllee <allen7575@gmail.com>
2017-11-30 22:48:46 +0800

    add my investment

--- gist/add_submodule.sh
+++ gist/add_submodule.sh
@@ -4,3 +4,4 @@
 # add items that you want it be a submodule
 #
 git submodule add https://gist.github.com/c764c86ed722417948fc256b7a5077c4.git ./import_json_appsscript
+git submodule add https://gist.github.com/43dc25a18ac687cfea7f5a64707b9a83.git ./my_investment
