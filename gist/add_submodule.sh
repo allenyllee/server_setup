c49e9fcbfc7de228b687a4b8809c6deeaf81eb83
allenyllee <allen7575@gmail.com>
2017-12-01 01:47:55 +0800

    add ubuntu_setup_from_git

--- gist/add_submodule.sh
+++ gist/add_submodule.sh
@@ -8,3 +8,4 @@ git submodule add https://gist.github.com/43dc25a18ac687cfea7f5a64707b9a83.git .
 git submodule add https://gist.github.com/5d7c4a16ae0e33375e4a6d25acaeeda2.git ./ppt2pdf
 git submodule add https://gist.github.com/ac42730e9c756e0aeb9b2f07424b68dd.git ./fetch_gdrive_file
 git submodule add https://gist.github.com/ddf9be045810572cd809ae3587a23658.git ./reverse_sshfs
+git submodule add https://gist.github.com/27e780b7ce7bd1248a754dff1ea3b011.git ./ubuntu_setup_from_git
