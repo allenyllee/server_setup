24b7c1f929f036b1dea0aa05616d727e175f5771
allenyllee <allen7575@gmail.com>
2017-12-01 01:45:10 +0800

    add reverse_sshfs

--- gist/add_submodule.sh
+++ gist/add_submodule.sh
@@ -7,3 +7,4 @@ git submodule add https://gist.github.com/c764c86ed722417948fc256b7a5077c4.git .
 git submodule add https://gist.github.com/43dc25a18ac687cfea7f5a64707b9a83.git ./my_investment
 git submodule add https://gist.github.com/5d7c4a16ae0e33375e4a6d25acaeeda2.git ./ppt2pdf
 git submodule add https://gist.github.com/ac42730e9c756e0aeb9b2f07424b68dd.git ./fetch_gdrive_file
+git submodule add https://gist.github.com/ddf9be045810572cd809ae3587a23658.git ./reverse_sshfs
