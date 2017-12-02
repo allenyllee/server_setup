d8e48d02324ad7c450d9ee52fa15c28244f8d65e
allenyllee <allen7575@gmail.com>
2017-12-02 17:06:50 +0800

    AllenYL_Coding_Exercises

--- repo/add_submodule.sh
+++ repo/add_submodule.sh
@@ -4,3 +4,4 @@
 # add items that you want it be a submodule
 #
 git submodule add https://github.com/allenyllee/Machine-Learning-Foundations.git
+git submodule add https://github.com/allenyllee/AllenYL_Coding_Exercises.git
