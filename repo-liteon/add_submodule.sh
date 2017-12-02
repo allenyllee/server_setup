2da6630f2d1877cac38d3ad8a43c06d3ff4e82a8
allenyllee <allen7575@gmail.com>
2017-12-02 21:59:17 +0800

    add liteon repo

--- repo-liteon/add_submodule.sh
+++ repo-liteon/add_submodule.sh
@@ -4,4 +4,6 @@
 # add items that you want it be a submodule
 #
 git submodule add https://gitlab.com/allenyllee/BELMAR.git
+git submodule add https://gitlab.com/allenyllee/EP3_TPE.git
+git submodule add https://gitlab.com/allenyllee/LiteonSSDFramework.git
 
