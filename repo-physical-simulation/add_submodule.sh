813c141fa081db987388b7608ca8afe8fdc724a3
allenyllee <allen7575@gmail.com>
2017-12-04 19:40:20 +0800

    add mjg-intel-fluid-demo

--- repo-physical-simulation/add_submodule.sh
+++ repo-physical-simulation/add_submodule.sh
@@ -5,3 +5,4 @@
 #
 git submodule add https://github.com/allenyllee/cloth-physics.git
 git submodule add https://github.com/allenyllee/fiedlers-cubes.git
+git submodule add https://github.com/allenyllee/mjg-intel-fluid-demo.git
