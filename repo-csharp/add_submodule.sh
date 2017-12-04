7b82ee301c23aabd8b806dfd3832d8925289cd6a
allenyllee <allen7575@gmail.com>
2017-12-04 18:26:57 +0800

    add display-brightness-console

--- repo-csharp/add_submodule.sh
+++ repo-csharp/add_submodule.sh
@@ -4,3 +4,4 @@
 # add items that you want it be a submodule
 #
 git submodule add https://github.com/allenyllee/learn-c-sharp.git
+git submodule add https://github.com/allenyllee/display-brightness-console.git
