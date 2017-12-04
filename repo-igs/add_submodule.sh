e12294b3baeb36fa90efca243e3303f3d0297d42
allenyllee <allen7575@gmail.com>
2017-12-04 17:19:58 +0800

    add master-server

--- repo-igs/add_submodule.sh
+++ repo-igs/add_submodule.sh
@@ -4,4 +4,5 @@
 # add items that you want it be a submodule
 #
 git submodule add -b clientServer https://allenyllee@bitbucket.org/allenyllee/cube-network-physics.git
+git submodule add https://allenyllee@bitbucket.org/allenyllee/master-server.git
 
