954010a1399b05ebc173ad3ccaa06b0f918eae51
allenyllee <allen7575@gmail.com>
2017-12-04 21:40:09 +0800

    add test-of-physx

--- repo-physical-simulation/add_submodule.sh
+++ repo-physical-simulation/add_submodule.sh
@@ -6,3 +6,4 @@
 git submodule add https://github.com/allenyllee/cloth-physics.git
 git submodule add https://github.com/allenyllee/fiedlers-cubes.git
 git submodule add https://github.com/allenyllee/mjg-intel-fluid-demo.git
+git submodule add https://github.com/allenyllee/test-of-physx.git
