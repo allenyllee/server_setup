db39c9c2d42af75400b4f7e9f438a9c2c2cf3a73
allenyllee <allen7575@gmail.com>
2017-12-04 18:08:37 +0800

    add rigidbody-constraint-demo2

--- repo-igs/add_submodule.sh
+++ repo-igs/add_submodule.sh
@@ -6,3 +6,4 @@
 git submodule add -b clientServer https://allenyllee@bitbucket.org/allenyllee/cube-network-physics.git
 git submodule add https://allenyllee@bitbucket.org/allenyllee/master-server.git
 git submodule add https://allenyllee@bitbucket.org/allenyllee/rigidbody-constraint-demo1.git
+git submodule add https://allenyllee@bitbucket.org/allenyllee/rigidbody-constraint-demo2.git
