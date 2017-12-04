98e99fe9d3e0aeaf741e10aa980aed28770cd6e9
allenyllee <allen7575@gmail.com>
2017-12-04 21:56:46 +0800

    add Box2D

--- repo-physical-simulation/add_submodule.sh
+++ repo-physical-simulation/add_submodule.sh
@@ -7,3 +7,4 @@ git submodule add https://github.com/allenyllee/cloth-physics.git
 git submodule add https://github.com/allenyllee/fiedlers-cubes.git
 git submodule add https://github.com/allenyllee/mjg-intel-fluid-demo.git
 git submodule add https://github.com/allenyllee/test-of-physx.git
+git submodule add https://github.com/allenyllee/Box2D.git
