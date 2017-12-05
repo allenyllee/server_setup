e2cba7dbbf0821bfb9aa39a8a0072473ef20bd42
allenyllee <allen7575@gmail.com>
2017-12-05 19:38:56 +0800

    add pega-bios

--- repo-pega/add_submodule.sh
+++ repo-pega/add_submodule.sh
@@ -6,4 +6,4 @@
 git submodule add https://github.com/allenyllee/pega-homework.git
 git submodule add https://allenyllee@bitbucket.org/allenyllee/pega-plog-parser.git
 git submodule add https://allenyllee@bitbucket.org/allenyllee/ezmake2.git
-
+git submodule add https://gitlab.com/allenyllee/pega-bios.git
