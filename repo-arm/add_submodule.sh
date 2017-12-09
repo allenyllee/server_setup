66ae8a7b5477f6bc4b1fdd711d8de9cf716d6c78
allenyllee <allen7575@gmail.com>
2017-12-09 18:29:15 +0800

    add raspberrypi

--- repo-arm/add_submodule.sh
+++ repo-arm/add_submodule.sh
@@ -4,4 +4,4 @@
 # add items that you want it be a submodule
 #
 git submodule add https://github.com/allenyllee/arm-tutorial-rpi.git
-
+git submodule add https://github.com/allenyllee/raspberrypi.git
