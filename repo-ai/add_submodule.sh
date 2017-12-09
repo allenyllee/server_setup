6a4b05dc57cd195056c5822775928d8f2e57396c
allenyllee <allen7575@gmail.com>
2017-12-09 17:10:32 +0800

    add AIIA-Level1-Aug

--- repo-ai/add_submodule.sh
+++ repo-ai/add_submodule.sh
@@ -4,4 +4,4 @@
 # add items that you want it be a submodule
 #
 git submodule add https://github.com/allenyllee/neural-networks-and-deep-learning.git
-
+git submodule add https://github.com/allenyllee/AIIA-Level1-Aug.git
