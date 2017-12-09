c0780642afbce5d9ef006a43396be84e55516f05
allenyllee <allen7575@gmail.com>
2017-12-09 18:42:44 +0800

    add zJiaJun.github.io

--- repo-press/add_submodule.sh
+++ repo-press/add_submodule.sh
@@ -4,4 +4,4 @@
 # add items that you want it be a submodule
 #
 git submodule add https://github.com/allenyllee/allenyllee.github.io.git
-
+git submodule add https://github.com/allenyllee/zJiaJun.github.io.git
