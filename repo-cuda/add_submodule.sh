1b47d4bd6ea21bbc47b806998fff0876b000a3ed
allenyllee <allen7575@gmail.com>
2017-12-09 17:26:49 +0800

    add CUDA_Samples_for_Windows

--- repo-cuda/add_submodule.sh
+++ repo-cuda/add_submodule.sh
@@ -4,3 +4,4 @@
 # add items that you want it be a submodule
 #
 git submodule add https://github.com/allenyllee/CompPhys.git
+git submodule add https://github.com/allenyllee/CUDA_Samples_for_Windows.git
