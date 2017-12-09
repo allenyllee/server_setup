ae100e6a0f7e18332a404e4ef73104c4c02efb94
allenyllee <allen7575@gmail.com>
2017-12-09 19:51:43 +0800

    add multipi

--- repo-arm/add_submodule.sh
+++ repo-arm/add_submodule.sh
@@ -6,3 +6,4 @@
 git submodule add https://github.com/allenyllee/arm-tutorial-rpi.git
 git submodule add https://github.com/allenyllee/raspberrypi.git
 git submodule add https://github.com/allenyllee/rpi-open-firmware.git
+git submodule add https://github.com/allenyllee/multipi.git
