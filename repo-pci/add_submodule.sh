78e63eca865f49d3e87b0dbf591613059a5bab84
allenyllee <allen7575@gmail.com>
2017-12-10 20:26:02 +0800

    add directio-0.2.1

--- repo-pci/add_submodule.sh
+++ repo-pci/add_submodule.sh
@@ -4,3 +4,4 @@
 # add items that you want it be a submodule
 #
 git submodule add -b windows-build https://github.com/allenyllee/pciutils.git
+git submodule add -f https://github.com/allenyllee/directio.git
