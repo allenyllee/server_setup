2991614c0defaf832b46e6e4e86eac6ac2fac646
allenyllee <allen7575@gmail.com>
2017-12-10 20:36:34 +0800

    add winio

--- repo-pci/add_submodule.sh
+++ repo-pci/add_submodule.sh
@@ -4,4 +4,5 @@
 # add items that you want it be a submodule
 #
 git submodule add -b windows-build https://github.com/allenyllee/pciutils.git
-git submodule add -f https://github.com/allenyllee/directio.git
+git submodule add https://github.com/allenyllee/directio.git
+git submodule add https://github.com/allenyllee/winio.git
