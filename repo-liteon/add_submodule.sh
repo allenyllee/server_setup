3312ecc67a84e98cdb37f3ee4d435c3a809be355
allenyllee <allen7575@gmail.com>
2017-12-11 22:59:38 +0800

    add liteon-ssd

--- repo-liteon/add_submodule.sh
+++ repo-liteon/add_submodule.sh
@@ -7,4 +7,4 @@ git submodule add https://gitlab.com/allenyllee/BELMAR.git
 git submodule add https://gitlab.com/allenyllee/EP3_TPE.git
 git submodule add https://gitlab.com/allenyllee/LiteonSSDFramework.git
 git submodule add -b xtensa https://gitlab.com/allenyllee/xtensa_X_docker.git
-
+git submodule add https://gitlab.com/allenyllee/liteon-ssd.git
