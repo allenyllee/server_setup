ffea87553fa8dc1df487a928b8d913475c88e7bb
allenyllee <allen7575@gmail.com>
2017-12-21 18:03:59 +0800

    add conv_arithmetic, Convolutional_Arithmetic

--- repo-ai/add_submodule.sh
+++ repo-ai/add_submodule.sh
@@ -6,3 +6,5 @@
 git submodule add https://github.com/allenyllee/neural-networks-and-deep-learning.git
 git submodule add https://github.com/allenyllee/AIIA-Level1-Aug.git
 git submodule add https://github.com/allenyllee/ganhacks.git
+git submodule add https://github.com/allenyllee/conv_arithmetic.git
+git submodule add https://gist.github.com/5bc27291d554d152e3f4aa1bd8008618.git ./Convolutional_Arithmetic
