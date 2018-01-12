43c82711a28f5d0b10a3c04ed840485bdd6ade75
allenyllee <allen7575@gmail.com>
2018-01-12 17:37:05 +0800

    add sonm-wallet

--- repo-blockchain/add_submodule.sh
+++ repo-blockchain/add_submodule.sh
@@ -5,4 +5,5 @@
 #
 git submodule add https://github.com/allenyllee/bitcoin-tool.git
 git submodule add https://gitlab.com/allenyllee/sonm.git
+git submodule add https://gitlab.com/allenyllee/sonm-wallet.io.git
 
