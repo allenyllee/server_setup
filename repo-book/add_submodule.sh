66f59050095395c575df1ef844c2170272fa9338
allenyllee <allen7575@gmail.com>
2018-02-19 12:09:05 +0800

    add book

--- repo-book/add_submodule.sh
+++ repo-book/add_submodule.sh
@@ -9,3 +9,4 @@ git submodule add https://gitlab.com/allenyllee/calibre-books.git
 git submodule add https://gitlab.com/allenyllee/books-n-articles.git
 git submodule add https://gitlab.com/allenyllee/sheet-music.git
 git submodule add https://gitlab.com/allenyllee/scrapbook.git
+git submodule add https://github.com/allenyllee/AllenYL-Notebook.git
