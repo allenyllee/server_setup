2f90aac491e77863b9cedb471eb05a67de7edbea
allenyllee <allen7575@gmail.com>
2018-02-25 17:15:24 +0800

    add ssd-gpu-dma & nvme-kmod

---
+++ repo-nvme/add_submodule.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# add items that you want it be a submodule
+#
+git submodule add https://github.com/allenyllee/ssd-gpu-dma.git
+git submodule add https://github.com/allenyllee/nvme-kmod.git
+
+
