1b9c012ff2fbaeffb5fa68c086f6e06df92e13db
allenyllee <allen7575@gmail.com>
2018-03-11 21:42:11 +0800

    add plugin-katex, HackMD-Book-Exporter-JS

--- repo-book/add_submodule.sh
+++ repo-book/add_submodule.sh
@@ -10,3 +10,5 @@ git submodule add https://gitlab.com/allenyllee/books-n-articles.git
 git submodule add https://gitlab.com/allenyllee/sheet-music.git
 git submodule add https://gitlab.com/allenyllee/scrapbook.git
 git submodule add https://github.com/allenyllee/AllenYL-Notebook.git
+git submodule add https://github.com/allenyllee/plugin-katex.git
+git submodule add https://github.com/allenyllee/HackMD-Book-Exporter-JS.git
