aea1de7086a52f8504126f7ee428870176502914
allenyllee <allen7575@gmail.com>
2018-05-05 13:13:03 +0800

    add books

--- repo-book/add_submodule.sh
+++ repo-book/add_submodule.sh
@@ -12,3 +12,5 @@ git submodule add https://gitlab.com/allenyllee/scrapbook.git
 git submodule add https://github.com/allenyllee/AllenYL-Notebook.git
 git submodule add https://github.com/allenyllee/plugin-katex.git
 git submodule add https://github.com/allenyllee/HackMD-Book-Exporter-JS.git
+git submodule add https://github.com/allenyllee/Parallel-Computing-Book.git
+git submodule add https://github.com/allenyllee/HPC-book-and-course.git
