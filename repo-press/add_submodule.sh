d6e94ec6961326260d975a3daa0ca478e9fa4bd5
allenyllee <allen7575@gmail.com>
2018-07-26 14:01:37 +0800

    git rm --cached allenyllee.github.io

--- repo-press/add_submodule.sh
+++ repo-press/add_submodule.sh
@@ -3,7 +3,6 @@
 #
 # add items that you want it be a submodule
 #
-git submodule add https://github.com/allenyllee/allenyllee.github.io.git
 git submodule add https://github.com/allenyllee/zJiaJun.github.io.git
 git submodule add https://gitlab.com/allenyllee/allenyllee.gitlab.io.git
 git submodule add https://github.com/allenyllee/karpathy.github.io.git
