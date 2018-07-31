1dcf9eccc81495a196316d56a642e8f4291601bb
allenyllee <allen7575@gmail.com>
2018-07-31 14:47:12 +0800

    add 2 docker dir, first locate at SSD, second at HDD

--- ubuntu_setup/run_services.sh
+++ ubuntu_setup/run_services.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
 
 PROJECT_DIR=$1
-DOCKERDIR="/mnt/docker-srv"
-DOCKERDIR2="$PROJECT_DIR/docker-srv"
+DOCKERDIR_SSD="/mnt/docker-srv"
+DOCKERDIR_HDD="$PROJECT_DIR/docker-srv"
 export DOCKERBIN="$(which docker)"
 
-sudo mkdir -p $DOCKERDIR
-sudo chmod 777 $DOCKERDIR
+sudo mkdir -p $DOCKERDIR_SSD
+sudo chmod 777 $DOCKERDIR_SSD
 
 #
 # gitlab data
 #
-export GITLAB_DIR=$DOCKERDIR2/gitlab
+export GITLAB_DIR=$DOCKERDIR_HDD/gitlab
 
 #
 # jenkins data
 #
 # security - How to give non-root user in Docker container access to a volume mounted on the host - Stack Overflow
 # https://stackoverflow.com/questions/39397548/how-to-give-non-root-user-in-docker-container-access-to-a-volume-mounted-on-the
-export JENKINS_DIR=$DOCKERDIR/jenkins_home
+export JENKINS_DIR=$DOCKERDIR_SSD/jenkins_home
 mkdir -p $JENKINS_DIR
 chown -R 1000:1000 $JENKINS_DIR
 
 #
 # docker registry data
 #
-export REGISTRY_DIR=$DOCKERDIR/registry
+export REGISTRY_DIR=$DOCKERDIR_SSD/registry
 export REG_BACKENDPORT=5000
 
 #
 # squid proxy data
 #
-export SQUID_DIR=$DOCKERDIR/squid/cache
+export SQUID_DIR=$DOCKERDIR_SSD/squid/cache
 
 #
 # samba server shared folder
 #
-export SAMBA_DIR=$DOCKERDIR2/samba
+export SAMBA_DIR_SSD=$DOCKERDIR_SSD/samba
+export SAMBA_DIR_HDD=$DOCKERDIR_HDD/samba
+
 
 #
 # xtensa source
@@ -47,7 +49,7 @@ export SAMBA_DIR=$DOCKERDIR2/samba
 export XTENSA_GIT=$(eval echo $PROJECT_DIR)/xtensa_X_docker
 
 # folder to share with samba
-export XTENSA_DIR=$SAMBA_DIR/xtensa_dir
+export XTENSA_DIR=$SAMBA_DIR_HDD/xtensa_dir
 
 ###################
 # docker
