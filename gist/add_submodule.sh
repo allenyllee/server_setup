9519936d5d584441859e88165647b31b7d5a554c
allenyllee <allen7575@gmail.com>
2018-08-03 12:25:16 +0800

    move ubuntu_setup_from_git to ubuntu_setup

--- gist/add_submodule.sh
+++ gist/add_submodule.sh
@@ -8,7 +8,6 @@ git submodule add https://gist.github.com/43dc25a18ac687cfea7f5a64707b9a83.git .
 git submodule add https://gist.github.com/5d7c4a16ae0e33375e4a6d25acaeeda2.git ./ppt2pdf
 git submodule add https://gist.github.com/ac42730e9c756e0aeb9b2f07424b68dd.git ./fetch_gdrive_file
 git submodule add https://gist.github.com/ddf9be045810572cd809ae3587a23658.git ./reverse_sshfs
-git submodule add https://gist.github.com/27e780b7ce7bd1248a754dff1ea3b011.git ./ubuntu_setup_from_git
 git submodule add https://gist.github.com/f7b0abce312ad210fbe6f9ad0061f1a3.git ./Pega_BIOS_Bring-Up
 git submodule add https://gist.github.com/d7431053780f469b30bfd4a073ebf711.git ./Linux_Driver_Coding_Notes
 git submodule add https://gist.github.com/9b692bb8e4d7f35d7848e78a28c8946c.git ./Timer-bat
---
+++ ubuntu_setup/scripts/add_submodule.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#
+# add items that you want it be a submodule
+#
+git submodule add https://gist.github.com/27e780b7ce7bd1248a754dff1ea3b011.git ./ubuntu_setup_from_git
