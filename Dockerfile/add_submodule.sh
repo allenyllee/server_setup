873b870c9d5b8f81a39fe276b744b90a777952ad
allenyllee <allen7575@gmail.com>
2018-08-11 22:29:58 +0800

    update

--- Dockerfile/add_submodule.sh
+++ Dockerfile/add_submodule.sh
@@ -3,8 +3,10 @@
 #
 # add items that you want it be a submodule
 #
-git submodule add https://github.com/allenyllee/condaD.git
 git submodule add https://github.com/allenyllee/motion-webcam-server.git
 git submodule add https://github.com/allenyllee/wine-base.git
 git submodule add https://github.com/allenyllee/cuda80-fix.git
 git submodule add https://github.com/allenyllee/docker-compassxport.git
+git submodule add https://github.com/allenyllee/cuda90-fix.git
+git submodule add https://github.com/allenyllee/anaconda3-fix.git
+git submodule add https://github.com/allenyllee/tensorflow-fix.git
