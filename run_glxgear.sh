0cdeeca250cec0293ec2d04eb3460c01ec6fe28a
allenyllee <allen7575@gmail.com>
2018-08-17 16:17:49 +0800

    update

--- run_glxgear.sh
+++ run_glxgear.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 docker build -t glxgears - << EOF
-FROM nvidia/cudagl:9.0-devel-ubuntu16.04
+FROM nvidia/cudagl:9.0-devel-ubuntu16.04  `# support OpenGL`
 RUN apt-get update
-RUN apt-get install -y mesa-utils x11-apps
-RUN apt-get update && \
-    apt-get install -y cmake && \
-    apt-get install -y xorg-dev libglu1-mesa-dev --fix-missing && \
-    apt-get install -y wget && \
-    `# apt-get install -y assimp-utils &&` \
-    `# apt-get install -y libassimp-dev &&` \
-    apt-get install -y libglfw3-dev && \
-    `# apt-get install -y libboost-all-dev &&` \
-    apt-get install -y git
+RUN apt-get install -y mesa-utils `# for glxgears` \
+                        x11-apps `# for xeyes` \
+                        wget `# for download file`
 
-RUN wget -O VulkanSDK.run https://sdk.lunarg.com/sdk/download/1.0.61.0/linux/vulkansdk-linux-x86_64-1.0.61.0.run?u= && \
+RUN apt-get install -y glmark2 `# for GPU benchmark` \
+                        lshw `# for lshw` \
+                        lsb-release `# for lsb_release` \
+                        software-properties-common python-software-properties `# for add-apt-repository ` \
+                        libvulkan1 vulkan-utils `# for vulkan` \
+                        libalut-dev `# for openal audio libaray` \
+                        xorg-dev `# avoid error while loading shared libraries: libXrandr.so.2`\
+                        libcurl3 `# avoid error while loading shared libraries: libcurl.so.4`
+
+########
+# install Vulkan SDK
+########
+
+RUN wget -O VulkanSDK.run https://sdk.lunarg.com/sdk/download/1.0.61.0/linux/vulkansdk-linux-x86_64-1.0.61.0.run?Human=true;u= && \
     chmod ugo+x VulkanSDK.run
 
 RUN ./VulkanSDK.run
 RUN cd VulkanSDK/1.0.61.0
-ENV VULKAN_SDK="/VulkanSDK/1.0.61.0/x86_64:${VULKAN_SDK}"
-ENV PATH="${VULKAN_SDK}/bin:${PATH}"
-ENV LD_LIBRARY_PATH="${VULKAN_SDK}/lib:${LD_LIBRARY_PATH}"
-ENV VK_LAYER_PATH="${VULKAN_SDK}/etc/explicit_layer.d:${VK_LAYER_PATH}"
+ENV VULKAN_SDK="/VulkanSDK/1.0.61.0/x86_64:\${VULKAN_SDK}" `# need to escape $ if you pass from shell script`
+ENV PATH="\${VULKAN_SDK}/bin:\${PATH}"
+ENV LD_LIBRARY_PATH="\${VULKAN_SDK}/lib:\${LD_LIBRARY_PATH}"
+ENV VK_LAYER_PATH="\${VULKAN_SDK}/etc/explicit_layer.d:\${VK_LAYER_PATH}"
 
-RUN apt-get install -y glmark2 \
-                        lshw `# for lshw` \
-                        lsb-release `# for lsb_release` \
-                        software-properties-common python-software-properties `# for add-apt-repository ` \
-                        libvulkan1 mesa-vulkan-drivers vulkan-utils `# for vulkan` \
-
-ENV QT_X11_NO_MITSHM=1 `# avoid BadDrawable (invalid Pixmap or Window parameter) issue when execute Unigine_Heaven-4.0`
+#ENV QT_X11_NO_MITSHM=1 `# avoid BadDrawable (invalid Pixmap or Window parameter) issue without MIT-SHM performance`
 
 CMD export LIBGL_DEBUG=verbose && glxgears
 EOF
@@ -47,5 +47,9 @@ nvidia-docker run -ti --rm \
     --volume=$XAUTH_DIR:$XAUTH_DIR \
     --env="DISPLAY=$DISPLAY" \
     --env="XAUTHORITY=$XAUTH" \
+    --env="PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native" `# for pulse audio backend`\
+    --volume=${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native `# for pulse audio backend` \
+    --device /dev/snd:/dev/snd `# for sound device`\
+    --ipc host `# avoid BadDrawable (invalid Pixmap or Window parameter) issue: with MIT-SHM performance`\
     --volume="/test:/test" \
     glxgears bash
