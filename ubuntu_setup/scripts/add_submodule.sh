2c4a221f0e041eac8c1eecd26e634c52d51574de
allenyllee <allen7575@gmail.com>
2018-08-22 11:31:07 +0800

    add autossh setup

--- ubuntu_setup/scripts/add_submodule.sh
+++ ubuntu_setup/scripts/add_submodule.sh
@@ -4,3 +4,4 @@
 # add items that you want it be a submodule
 #
 git submodule add https://gist.github.com/27e780b7ce7bd1248a754dff1ea3b011.git ./ubuntu_setup_from_git
+git submodule add https://gist.github.com/a1298c08dcd0b92ad9138e0faef979f3.git ./autossh_setup
