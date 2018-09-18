ba43aecf8937688dab770775bce8b6050ac9f577
allenyllee <allen7575@gmail.com>
2018-09-18 22:51:33 +0800

    add mount vhdx

--- ubuntu_setup/scripts/add_submodule.sh
+++ ubuntu_setup/scripts/add_submodule.sh
@@ -5,3 +5,4 @@
 #
 git submodule add https://gist.github.com/27e780b7ce7bd1248a754dff1ea3b011.git ./ubuntu_setup_from_git
 git submodule add https://gist.github.com/a1298c08dcd0b92ad9138e0faef979f3.git ./autossh_setup
+git submodule add https://gist.github.com/0a4c02952bf695470860b27369bbb60d.git ./mount_vhdx
