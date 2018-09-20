3711b4dd66c1a695961f530cc67272fe3025c5c7
allenyllee <allen7575@gmail.com>
2018-09-20 17:43:43 +0800

    add set networking script

--- ubuntu_setup/scripts/add_submodule.sh
+++ ubuntu_setup/scripts/add_submodule.sh
@@ -6,3 +6,4 @@
 git submodule add https://gist.github.com/27e780b7ce7bd1248a754dff1ea3b011.git ./ubuntu_setup_from_git
 git submodule add https://gist.github.com/a1298c08dcd0b92ad9138e0faef979f3.git ./autossh_setup
 git submodule add https://gist.github.com/0a4c02952bf695470860b27369bbb60d.git ./mount_vhdx
+git submodule add https://gist.github.com/c11c150842578d514c6a46caaaa7e1ad.git ./set_networking
