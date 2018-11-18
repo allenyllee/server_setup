9c0eca62727d8524cadee2e433a119fb8ec77c58
allenyllee <allen7575@gmail.com>
2018-11-18 14:03:48 +0800

    update

--- ubuntu_setup/deluge/run_deluge.sh
+++ ubuntu_setup/deluge/run_deluge.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # mount data folder
-#sudo mkdir /downloads
+sudo mkdir /downloads
 sudo mount --bind ~/Projects/ResilioSyncFolder/Media\(btsync\)/ /downloads
 #sudo ln -s ~/Projects/ResilioSyncFolder/Media\(btsync\)/.deluge ~/.config/deluge
 # mount config folder
+sudo mkdir /downloads2
+sudo mount --bind ~/Projects/ResilioSyncFolder/BT/ /downloads2
 
 sudo mount --bind ~/Projects_SSD/docker-srv/DelugeConfig ~/.config/deluge
 #sudo ln -s ~/Projects/ResilioSyncFolder/Media\(btsync\) /downloads
