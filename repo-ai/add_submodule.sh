556842660337a91ced9079ba1a90ad5924842589
allenyllee <allen7575@gmail.com>
2019-05-16 14:37:39 +0800

    update nlp-ai-in-law url

--- repo-ai/add_submodule.sh
+++ repo-ai/add_submodule.sh
@@ -10,7 +10,7 @@ git submodule add https://github.com/allenyllee/conv_arithmetic.git
 git submodule add https://gist.github.com/5bc27291d554d152e3f4aa1bd8008618.git ./Convolutional_Arithmetic
 git submodule add https://github.com/allenyllee/neural-nets.git
 git submodule add https://github.com/allenyllee/AIacademy.git
-git submodule add https://gitlab.com/allenyllee/NLP-AI-in-Law.git
+git submodule add https://gitlab.com/AIFR/ai-in-law/NLP-AI-in-Law.git
 git submodule add https://github.com/allenyllee/Python-in-5-days.git
 git submodule add https://github.com/allenyllee/gensim.git
-git submodule add https://gitlab.com/allenyllee/nlp-ai-in-law-exercise.git
+git submodule add https://gitlab.com/AIFR/ai-in-law/nlp-ai-in-law-exercise.git
