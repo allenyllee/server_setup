888d1cce704f091074a97558f4dd1c70e34cc851
allenyllee <allen7575@gmail.com>
2019-05-28 12:22:43 +0800

    update

--- ubuntu_setup/ubuntu_setup.sh
+++ ubuntu_setup/ubuntu_setup.sh
@@ -890,14 +890,16 @@ then
 # remove offical ppa first
 sudo add-apt-repository -r ppa:i-nex-development-team/stable
 # for ubuntu 18.04 (unoffical)
+# I-Nex PPA for Ubuntu 18/Mint 19? · Issue #89 · i-nex/I-Nex
+# https://github.com/i-nex/I-Nex/issues/89
 sudo add-apt-repository -y ppa:trebelnik-stefina/i-nex
+# i-nex depends on gambas3
+sudo add-apt-repository -y ppa:gambas-team/gambas3
 else
 # for ubuntu 16.04 (offical)
 sudo add-apt-repository -y ppa:i-nex-development-team/stable
 fi
 
-# i-nex depends on gambas3
-sudo add-apt-repository -y ppa:gambas-team/gambas3
 sudo apt-get update
 sudo apt-get install -y i-nex
 
