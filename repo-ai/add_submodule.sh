acb7be0f7ebf66ba27f43b22b28487d099efab6d
allenyllee <allen7575@gmail.com>
2020-09-04 22:01:48 +0800

    add fxxk-nthu-post-classify

--- repo-ai/add_submodule.sh
+++ repo-ai/add_submodule.sh
@@ -20,3 +20,4 @@ git submodule add https://github.com/allenyllee/ML_exercise.git
 git submodule add https://gitlab.com/AIFR/ai-in-law/nlp-judgement-frontend.git
 git submodule add https://gitlab.com/AIFR/ai-in-law/custody-prediction-modeling.git
 git submodule add https://gitlab.com/AIFR/nlp/suicide-text-detection.git
+git submodule add https://gitlab.com/AIFR/nlp/fxxk-nthu-post-classify.git
