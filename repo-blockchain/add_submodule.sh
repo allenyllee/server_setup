b1f6602e185e686983c4b4f821bbd62f536bf60f
allenyllee <allen7575@gmail.com>
2021-08-30 18:51:41 +0800

    add submodule locallogm

--- repo-blockchain/add_submodule.sh
+++ repo-blockchain/add_submodule.sh
@@ -7,4 +7,4 @@ git submodule add https://github.com/allenyllee/bitcoin-tool.git
 git submodule add https://gitlab.com/allenyllee/sonm.git
 git submodule add https://gitlab.com/allenyllee/sonm-wallet.io.git
 git submodule add https://github.com/allenyllee/StorjNode.git
-
+git submodule add https://github.com/allenyllee/locallogm.git
